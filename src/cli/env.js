7b765ea39f7808af24c79f5bd76facf90fcfb2a1
YuliaBondar <uliabondar359@gmail.com>
2024-10-02 19:27:16 +0300

    add cli files

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVariables = process.env;
+
+    const rssVars = Object.keys(envVariables)
+        .filter(key => key.startsWith('RSS_'))
+        .map(key => `${key}=${envVariables[key]}`); 
+    const result = rssVars.join('; ');
+
+    console.log(result);
 };
 
 parseEnv();
